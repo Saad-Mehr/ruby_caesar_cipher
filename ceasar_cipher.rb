c29ccdd84683c9b208041efc30b8fadf1323ecdc
Saad Mehr <saad.mehr.635@my.csun.edu>
2022-08-21 12:56:21 -0700

    refactored shit

--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -1,24 +1,22 @@
 require 'pry-byebug'
 
 def ceasar_cipher(decrypted_text, shift)
-    binding.pry
-    byte_array = decrypted_text.bytes
-    new_array = []
-    byte_array.each do |bytey|
-        if bytey >= 65 && bytey <= 90
-            bytey += shift
-            if bytey > 90
-                bytey -= 26
+    encrypted_byte_array = []
+    decrypted_text.each_byte do |byte|
+        if byte >= 65 && byte <= 90
+            byte += shift
+            if byte > 90
+                byte -= 26
             end
-        elsif bytey >= 95 && bytey <= 122
-            bytey += shift
-                if bytey > 122
-                    bytey -= 26
+        elsif byte >= 95 && byte <= 122
+            byte += shift
+                if byte > 122
+                    byte -= 26
                 end
         end
-        new_array.push(bytey)
+        new_array.push(byte)
     end
     encrypted_text = new_array.pack('c*')
     return encrypted_text
 end
-
+git
